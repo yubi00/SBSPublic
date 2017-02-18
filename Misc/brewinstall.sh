07d355470d335df5f316f9d10e1d54bfaca8ff43
Guy Harrison <guy.a.harrison@gmail.com>
2017-02-18 16:50:58 +1100

    Brew install script

---
+++ Misc/brewinstall.sh
@@ -0,0 +1,8 @@
+xcode-select --install
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install caskroom/cask/brew-cask
+brew install  emacs gdbm git gmp gnutls icu4c jasper jpeg libtasn1 libtool lynx maven scala trash thefuck
+brew install mongodb mysql nettle node nvm openssl perl postgresql python readline socat sqlite wget
+brew install apache-drill gcc
+brew cask install adobe-reader iterm2 skype java
+brew cask install atom docker dropbox evernote google-chrome intellij-idea slack spectacle vagrant virtualbox
