13021e1e702d64a5e1e71bc4c62c520bce3ff357
Guy Harrison <guy.a.harrison@gmail.com>
2017-03-29 16:28:53 +1100

    vscode

--- Misc/brewinstall.sh
+++ Misc/brewinstall.sh
@@ -2,10 +2,10 @@ xcode-select --install
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install caskroom/cask/brew-cask
 brew install  emacs gdbm git gmp gnutls icu4c jasper jpeg libtasn1 libtool lynx maven scala trash thefuck
-brew install mongodb mysql nettle node nvm openssl perl postgresql python readline socat sqlite wget
+brew install mongodb mysql nettle node npm nvm openssl perl postgresql python readline socat sqlite wget
 brew install apache-drill gcc docker maven
 brew cask install adobe-reader iterm2 skype java
 brew cask install atom docker dropbox evernote google-chrome intellij-idea slack spectacle vagrant virtualbox
-brew cask install adobe-creative-cloud sketch
+brew cask install adobe-creative-cloud sketch visual-studio-code
 
 
